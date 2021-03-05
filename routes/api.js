d22d61a010c49ad77ec23103170b511495628bb6
Tim Scott <0timothy.scott0@gmail.com>
2021-03-04 21:23:19 -0500

    api post note

--- routes/api.js
+++ routes/api.js
@@ -10,5 +10,27 @@ router.route("/notes")
 	.get((req, res) => {
 		res.sendFile("db.json", { root: dbPath });
 	})
+	.post((req, res) => {
+		// make sure note has title and text properties
+		const note = { title, text } = req.body;
+		// create an id
+		note.id = uuidv4();
+		// send status
+		res.sendStatus(202);
+		// update db
+		fsPromise.readFile(path.join(dbPath, "db.json"), "utf8")
+			.then((data) => {
+				// parse data
+				data = JSON.parse(data);
+				// make sure data is an array
+				if (!Array.isArray(data)) data = [];
+				// add new note
+				data.push(note);
+				// save note
+				return fsPromise.writeFile(
+					path.join(dbPath, "db.json"),
+					JSON.stringify(data, null, "	"))
+			}).catch(console.error)
+	})
 
 module.exports = router;
